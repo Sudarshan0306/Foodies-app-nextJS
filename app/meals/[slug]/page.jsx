804ac444c0319ef03d8cdff1775890c582b8acb1
sudarshan.shinde <sudarshan.shinde@inadev.com>
2024-09-07 22:19:01 +0530

    added static and dynamic routes

---
+++ app/meals/[slug]/page.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const MealsDetailPage = ({params}) => {
+  return (
+    <main>
+      <h1>MealsDetailPage</h1>
+      <p>
+        {params.slug}
+      </p>
+    </main>
+  );
+};
+
+export default MealsDetailPage;
---
+++ app/meals/share/page.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const SharePage = () => {
+  return (
+    <main>
+      <h1>SharePage</h1>
+    </main>
+  );
+};
+
+export default SharePage;
