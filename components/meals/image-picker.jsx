2c9da16b37084d1ca1ae164b31fa974dc0a3196f
sudarshan.shinde <sudarshan.shinde@inadev.com>
2024-09-12 17:08:23 +0530

    complete image picker and preview done

--- components/meals/image-picker.jsx
+++ components/meals/image-picker.jsx
@@ -1,26 +1,59 @@
-'use client';
-  
-import React, { useRef } from "react";
+"use client";
+
+import React, { useRef, useState } from "react";
 import classes from "./image-picker.module.css";
+import Image from "next/image";
 
 const ImagePicker = ({ label, name }) => {
+  const [pickedImage, setPickedImage] = useState();
   const fileRef = useRef();
   const handlePickClick = () => {
     fileRef.current.click();
-  }
+  };
+  const handleImageChange = (event) => {
+    const file = event.target.files[0];
+
+    if (!file) {
+      setPickedImage(null);
+      return;
+    }
+    const fileReader = new FileReader();
+
+    fileReader.onload = () => {
+      setPickedImage(fileReader.result);
+    };
+
+    fileReader.readAsDataURL(file);
+  };
   return (
     <div className={classes.picker}>
-      <label htmlFor={name}></label>
+      <label htmlFor={name}>{label}</label>
       <div className={classes.controls}>
+        <div className={classes.preview}>
+          {!pickedImage && <p>No image picked yet.</p>}
+          {pickedImage && (
+            <Image
+              src={pickedImage}
+              alt="The image selected by the user."
+              fill
+            />
+          )}
+        </div>
         <input
           type="file"
           name={name}
           id={name}
           className={classes.input}
-          accept="image/png, image/jpeg"
+          accept="image/png, image/jpeg, image/jpg"
           ref={fileRef}
+          onChange={handleImageChange}
+          required
         />
-        <button className={classes.button} type="button" onClick={handlePickClick}>
+        <button
+          className={classes.button}
+          type="button"
+          onClick={handlePickClick}
+        >
           Pick an image
         </button>
       </div>
