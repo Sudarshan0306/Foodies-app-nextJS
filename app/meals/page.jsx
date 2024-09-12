7b8cf9afa9dc82d6d9f9a8e3a75f4d6b4bb00a77
sudarshan.shinde <sudarshan.shinde@inadev.com>
2024-09-12 10:31:34 +0530

    added loading state  using suspense

--- app/meals/page.jsx
+++ app/meals/page.jsx
@@ -1,26 +1,31 @@
 import Link from "next/link";
-import React from "react";
-import classes from './page.module.css';
+import React, { Suspense } from "react";
+import classes from "./page.module.css";
 import MealsGrid from "@/components/meals/meals-grid";
 import { getMeals } from "@/lib/meals";
 
-const MealsPage = async () => {
+const Meals = async () => {
   const meals = await getMeals();
+  return <MealsGrid meals={meals} />;
+};
+
+const MealsPage = () => {
   return (
     <>
       <header className={classes.header}>
         <h1>
-          Delicious meals, created <span className={classes.highlight}> by you </span>
+          Delicious meals, created{" "}
+          <span className={classes.highlight}> by you </span>
         </h1>
-        <p>
-          Choose your favorite recipe and cook yourself. It's east and fun.
-        </p>
+        <p>Choose your favorite recipe and cook yourself. It's east and fun.</p>
         <p className={classes.cta}>
-          <Link href='/meals/share'>Share Your Favorite Recipe</Link>
+          <Link href="/meals/share">Share Your Favorite Recipe</Link>
         </p>
       </header>
       <main className={classes.main}>
-        <MealsGrid meals={meals} />  
+        <Suspense fallback={<p className={classes.loading}>Fetching meals...</p>}>
+        <Meals />
+        </Suspense>
       </main>
     </>
   );
