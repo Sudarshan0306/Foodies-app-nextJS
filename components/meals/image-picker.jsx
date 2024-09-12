183471b4352e217a1ab818d794e95a06d59a93f7
sudarshan.shinde <sudarshan.shinde@inadev.com>
2024-09-12 16:17:40 +0530

    share page form and complete UI

--- components/meals/image-picker.jsx
+++ components/meals/image-picker.jsx
@@ -1,10 +1,31 @@
-import React from 'react';
-import classes from './image-picker.module.css';
+'use client';
+  
+import React, { useRef } from "react";
+import classes from "./image-picker.module.css";
 
-const ImagePicker = () => {
+const ImagePicker = ({ label, name }) => {
+  const fileRef = useRef();
+  const handlePickClick = () => {
+    fileRef.current.click();
+  }
   return (
-    <div>ImagePicker</div>
-  )
-}
+    <div className={classes.picker}>
+      <label htmlFor={name}></label>
+      <div className={classes.controls}>
+        <input
+          type="file"
+          name={name}
+          id={name}
+          className={classes.input}
+          accept="image/png, image/jpeg"
+          ref={fileRef}
+        />
+        <button className={classes.button} type="button" onClick={handlePickClick}>
+          Pick an image
+        </button>
+      </div>
+    </div>
+  );
+};
 
-export default ImagePicker
+export default ImagePicker;
