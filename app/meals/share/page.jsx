a18a4d1a515e5edc8606c2f9d150ce3fe9b521e2
sudarshan.shinde <sudarshan.shinde@inadev.com>
2024-09-13 20:41:22 +0530

    added one function to send data to backend

--- app/meals/share/page.jsx
+++ app/meals/share/page.jsx
@@ -1,8 +1,20 @@
 import React from "react";
-import classes from './page.module.css'
+import classes from "./page.module.css";
 import ImagePicker from "@/components/meals/image-picker";
 
 const SharePage = () => {
+  const shareMeal = async (formData) => {
+    "use server";
+
+    const meal = {
+      title: formData.get("title"),
+      summary: formData.get("summary"),
+      instructions: formData.get("instructions"),
+      image: formData.get("image"),
+      creator: formData.get("title"),
+      creator_email: formData.get("creator_email"),
+    };
+  };
   return (
     <>
       <header className={classes.header}>
@@ -12,7 +24,7 @@ const SharePage = () => {
         <p>Or any other meal you feel needs sharing!</p>
       </header>
       <main className={classes.main}>
-        <form className={classes.form}>
+        <form className={classes.form} action={shareMeal}>
           <div className={classes.row}>
             <p>
               <label htmlFor="name">Your name</label>
