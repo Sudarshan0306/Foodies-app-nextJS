a596ef76c189a7ca85c4e818bcc78075f0ceeee6
sudarshan.shinde <sudarshan.shinde@inadev.com>
2024-09-17 22:45:38 +0530

    ui fix

--- app/meals/page.jsx
+++ app/meals/page.jsx
@@ -14,7 +14,7 @@ const MealsPage = () => {
     <>
       <header className={classes.header}>
         <h1>
-          Delicious meals, created{" "}
+          Delicious meals, created
           <span className={classes.highlight}> by you </span>
         </h1>
         <p>Choose your favorite recipe and cook yourself. Its east and fun.</p>
